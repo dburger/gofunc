14d641c50c8c74d87793daa95375f75f1bcf3099
David J. Burger <davidburger@gmail.com>
2023-09-12 07:04:08 -0500

    Add godoc comments, some renaming in tests

--- functional.go
+++ functional.go
@@ -1,31 +1,37 @@
+/*
+Package gofunc provides functional primitives.
+*/
 package gofunc
 
-func Filter[T any](slice []T, pred func(T) bool) []T {
+// Filter returns a new slice of only the items that pass predicate.
+func Filter[T any](items []T, predicate func(T) bool) []T {
 	result := make([]T, 0)
-	for _, v := range slice {
-		if pred(v) {
+	for _, v := range items {
+		if predicate(v) {
 			result = append(result, v)
 		}
 	}
 	return result
 }
 
-func Map[T any, U any](slice []T, mapper func(T) U) []U {
-	result := make([]U, len(slice))
-	for i, v := range slice {
+// Map returns a new slice of items transformed by mapper.
+func Map[T any, U any](items []T, mapper func(T) U) []U {
+	result := make([]U, len(items))
+	for i, v := range items {
 		result[i] = mapper(v)
 	}
 	return result
 }
 
-func FlatMap[T any, U any](slice [][]T, mapper func(T) U) []U {
+// FlatMap returns a new slice of items mapped and flattened into a single dimensional slice.
+func FlatMap[T any, U any](items [][]T, mapper func(T) U) []U {
 	size := 0
-	for _, l := range slice {
+	for _, l := range items {
 		size += len(l)
 	}
 	result := make([]U, size)
 	i := 0
-	for _, l := range slice {
+	for _, l := range items {
 		for _, v := range l {
 			result[i] = mapper(v)
 			i++
@@ -34,9 +40,13 @@ func FlatMap[T any, U any](slice [][]T, mapper func(T) U) []U {
 	return result
 }
 
-func Reduce[T any, U any](slice []T, comb func(a U, b T) U, first U) U {
-	for _, v := range slice {
-		first = comb(first, v)
+// Reduce combines items using a combiner function reducing them to a single value.
+// Each item is combined with the result of the combiner function operating on the
+// prior item. A first parameter allows the initial value for the combiner to be
+// specified.
+func Reduce[T any, U any](items []T, combiner func(a U, b T) U, first U) U {
+	for _, v := range items {
+		first = combiner(first, v)
 	}
 	return first
 }
--- functional_test.go
+++ functional_test.go
@@ -7,34 +7,34 @@ import (
 )
 
 func TestFilter(t *testing.T) {
-	list := []int{1, 2, 3, 4}
-	list = Filter(list, func(i int) bool { return i%2 == 0 })
-	assert.Equal(t, []int{2, 4}, list)
+	items := []int{1, 2, 3, 4}
+	items = Filter(items, func(i int) bool { return i%2 == 0 })
+	assert.Equal(t, []int{2, 4}, items)
 }
 
 func TestMap(t *testing.T) {
-	list := []int{1, 2, 3, 4}
-	list = Map(list, func(i int) int { return i * 2 })
-	assert.Equal(t, []int{2, 4, 6, 8}, list)
+	items := []int{1, 2, 3, 4}
+	items = Map(items, func(i int) int { return i * 2 })
+	assert.Equal(t, []int{2, 4, 6, 8}, items)
 }
 
 func TestFlatmap(t *testing.T) {
-	list := [][]int{
-		[]int{1, 2, 3},
-		[]int{4, 5},
-		[]int{6},
+	items := [][]int{
+		{1, 2, 3},
+		{4, 5},
+		{6},
 	}
-	flattened := FlatMap(list, func(i int) int {
+	flattened := FlatMap(items, func(i int) int {
 		return i * 2
 	})
 	assert.Equal(t, []int{2, 4, 6, 8, 10, 12}, flattened)
 }
 
 func TestReduce(t *testing.T) {
-	list := []int{1, 2, 3}
-	result := Reduce(list, func(a, b int) int { return a + b }, 0)
+	items := []int{1, 2, 3}
+	result := Reduce(items, func(a, b int) int { return a + b }, 0)
 	assert.Equal(t, 6, result)
 
-	result = Reduce(list, func(a, b int) int { return a * b }, 1)
+	result = Reduce(items, func(a, b int) int { return a * b }, 1)
 	assert.Equal(t, 6, result)
 }
